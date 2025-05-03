09f8882fa83793f23b0247311081f1818711eb10
YoussefUMU <youssefumu@gmail.com>
2025-05-03 19:06:44 +0200

    Detalles

--- ProyectoPDS/src/main/java/modelado/EstrategiaAleatoria.java
+++ ProyectoPDS/src/main/java/modelado/EstrategiaAleatoria.java
@@ -1,7 +1,7 @@
 package modelado;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,38 +12,38 @@ public class EstrategiaAleatoria implements Estrategia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	
-	@Override
-	public Bloque siguienteBloque(List<Bloque> bloques, int actual, List<Bloque> bloquesCompletos) {
-		// Lista de bloques disponibles que cumplen las condiciones
-		List<Bloque> bloquesDisponibles = bloques.stream()
-				.filter(bloque -> !bloquesCompletos.contains(bloque) && !bloque.equals(bloques.get(actual))).toList();
 
-		// Si no hay bloques disponibles, devuelve null
-		if (bloquesDisponibles.isEmpty()) {
-			return null;
-		}
+    @Override
+    public Bloque siguienteBloque(List<Bloque> bloques, int actual, List<Bloque> bloquesCompletos) {
+        // Lista de bloques disponibles que cumplen las condiciones
+        List<Bloque> bloquesDisponibles = bloques.stream()
+                .filter(bloque -> !bloquesCompletos.contains(bloque) && !bloque.equals(bloques.get(actual)))
+                .collect(Collectors.toList());
 
-		// Selecciona un bloque aleatoriamente
-		int indiceAleatorio = (int) (Math.random() * bloquesDisponibles.size());
-		return bloquesDisponibles.get(indiceAleatorio);
-	}
+        // Si no hay bloques disponibles, devuelve null
+        if (bloquesDisponibles.isEmpty()) {
+            return null;
+        }
 
-	@Override
-	public Pregunta siguientePregunta(Bloque Bactual, int Pactual, List<Pregunta> PreguntasCompletas) {
-		// Filtrar las preguntas disponibles en el bloque actual
-		List<Pregunta> preguntasDisponibles = Bactual.getPreguntas().stream()
-				.filter(pregunta -> !PreguntasCompletas.contains(pregunta) && pregunta.getNumPregunta() != Pactual)
-				.toList();
+        // Selecciona un bloque aleatoriamente
+        int indiceAleatorio = (int) (Math.random() * bloquesDisponibles.size());
+        return bloquesDisponibles.get(indiceAleatorio);
+    }
 
-		// Si no hay preguntas disponibles, devolver null
-		if (preguntasDisponibles.isEmpty()) {
-			return null;
-		}
+    @Override
+    public Pregunta siguientePregunta(Bloque Bactual, int Pactual, List<Pregunta> PreguntasCompletas) {
+        // Filtrar las preguntas disponibles en el bloque actual
+        List<Pregunta> preguntasDisponibles = Bactual.getPreguntas().stream()
+                .filter(pregunta -> !PreguntasCompletas.contains(pregunta) && pregunta.getNumPregunta() != Pactual)
+                .collect(Collectors.toList());
 
-		// Seleccionar una pregunta aleatoriamente
-		int indiceAleatorio = (int) (Math.random() * preguntasDisponibles.size());
-		return preguntasDisponibles.get(indiceAleatorio);
-	}
+        // Si no hay preguntas disponibles, devolver null
+        if (preguntasDisponibles.isEmpty()) {
+            return null;
+        }
 
+        // Seleccionar una pregunta aleatoriamente
+        int indiceAleatorio = (int) (Math.random() * preguntasDisponibles.size());
+        return preguntasDisponibles.get(indiceAleatorio);
+    }
 }
